0514b34dca0ee39e8c4c81e31bd583d38c619eb1
Kjell-Magne <kjell_m.larsen@yahoo.com>
2025-01-11 12:46:17 +0100

    JS refaktor og kommentarer

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,51 @@
 
-const nav = resources.map(e => {
+// Setter opp tabs
+document.getElementsByTagName("ul")[0].innerHTML = resources.map(e => {
     return  `<li class="tab">${e.category}</li>`
-})
+}).join('')
 
 const domArticle = document.getElementsByTagName("article")[0]
-
-document.getElementsByTagName("ul")[0].innerHTML = nav.join('');
 const domTabs = document.querySelectorAll(".tab")
-currentTab = domTabs[0];
-domArticle.innerHTML = getResource(resources, currentTab);
-toggleTab(0)
+let currentTab;
 
-domTabs.forEach((tab, index) => {
+// Instansierer tab ved innlastning
+toggleTabs(domTabs[0])
+domArticle.innerHTML = getResource(resources, currentTab.textContent);
+
+console.log(typeof currentTab)
+
+// Setter opp eventlistner for hver tab
+domTabs.forEach((tab) => {
     tab.addEventListener("click", event => {
-        console.log(tab.classList)
-        toggleTab(index);
-       currentTab = domTabs[index]
-       domArticle.innerHTML = getResource(resources, currentTab);
-    
+        toggleTabs(tab);
+        domArticle.innerHTML = getResource(resources, currentTab.textContent)
     })
 })
 
-function getResource(objects, selected) {
+console.log(typeof resources)
+
+// Sjekker først om currentTab holder et objekt og fjerner det
+// Setter nytt aktivt objekt
+function toggleTabs(newTab) {
+    if(currentTab) {
+        currentTab.classList.remove("tab_active")
+    }
+    currentTab = newTab
+    currentTab.classList.add("tab_active")
+}
+
+// filtrert objekt basert på en string
+function getResource(objects, value) {
     return objects.filter(resource => {
-        return resource.category === selected.textContent
+        return resource.category === value
     }).map(resource => {return articleTemplate(resource)})
 }
 
+// mal for html kode i et objekt
 function articleTemplate(object) {
     return `<h2>${object.category}</h2>
             <p>${object.text}</p>
             <ul>
                 ${object.sources.map(link => `<li><a href="${link.url}">${link.title}</a></li>`).join('')}
             </ul>`
-}
-
-function toggleTab(index) {
-    currentTab.classList.remove("tab_active")
-    currentTab = domTabs[index]
-    currentTab.classList.add("tab_active")
-
-    //object.classList.add("tab_active")
 }
